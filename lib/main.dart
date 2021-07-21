86f9c0f385d30f43c69808ed2864356e22ac8d9a
passtimecoder <hegderdivya@gmail.com>
2021-07-21 11:17:36 +0530

    routes and themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ratecourse/home.dart';
+import 'package:ratecourse/pages/home.dart';
+import 'package:ratecourse/pages/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => Login(),
+        "/home": (context) => Home(),
+        "/login": (context) => Login(),
+      },
     );
   }
 }
